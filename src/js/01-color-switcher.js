8e1a3b9e42a63c422c71c0dd391284677a9fba7d
YuriiPohorilets <pohorilets.y@gmail.com>
2022-08-26 11:55:15 +0300

    task #1 done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+const refs = {
+  start: document.querySelector('[data-start'),
+  stop: document.querySelector('[data-stop'),
+};
+
+refs.stop.disabled = true;
+
+const INTERVAL_DURATION = 1_000;
+let timerlId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnClickStart() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
+  timerlId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DURATION);
+}
+
+function onBtnClickStop() {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+
+  clearInterval(timerlId);
+}
+
+refs.start.addEventListener('click', onBtnClickStart);
+refs.stop.addEventListener('click', onBtnClickStop);
